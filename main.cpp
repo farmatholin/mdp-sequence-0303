d2720f1757df3cc57c1a66d3a23380ccd6d7c037
Vladislav <farmatholin@gmail.com>
2015-05-17 21:06:56 +0300

    update testing methods

--- main.cpp
+++ main.cpp
@@ -12,124 +12,23 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-
-    //Testting -------------------------------------------------
-    // ListItem Test
-    //=================================================
-    ListItem<int>* li = new ListItem<int>(5);
-    printf("ListItem = %d\n", li->getValue());
-    li->setValue(10);
-    printf("ListItem = %d\n", li->getValue());
-    if (li->getNext() != NULL){
-        printf("ListItem = %d\n", li->getNext()->getValue());
-    }
-    else{
-        printf("li->getNext()==NULL\n");
-    }
-    li->setNext(li);
-    if (li->getNext() != NULL){
-        printf("ListItem = %d\n", li->getNext()->getValue());
-    }
-    else{
-        printf("li->getNext()==NULL\n");
-    }
-    delete(li);
-    //=================================================
-    printf("\n===================================================\n");
-
-    List<int>* list = new List<int>();
-    list->push_back(1);
-    printf("ADD %d ", list->at(0));
-    list->push_back(2);
-    printf("ADD %d ", list->at(1));
-    list->push_back(3);
-    printf("ADD %d ", list->at(2));
-    list->push_back(4);
-    printf("ADD %d ", list->at(3));
-    list->push_back(5);
-    printf("ADD %d ", list->at(4));
-    list->push_back(6);
-    printf("ADD %d ", list->at(5));
-    printf("\n");
-    for (int i = 0; i<list->size(); i++){
-        printf("%d ", list->at(i));
-    }
-    list->popAt(0);
-    list->popAt(4);
-    list->popAt(3);
-    printf("\n");
-    for (int i = 0; i<list->size(); i++){
-        printf("%d ", list->at(i));
-    }
-    delete(list);
-
-    printf("\n===================================================\n");
-
-    printf("\n===================Test Entity======================\n");
-
-    List<Entity*>* entitieList = new List<Entity*>();
-    entitieList->push_back(new Entity("123"));
-    printf("ADD Entity %s \n", entitieList->at(0)->getID().c_str());
-
-    entitieList->push_back(new Entity("GAGA"));
-    printf("ADD Entity %s \n", entitieList->at(1)->getID().c_str());
-
-    entitieList->push_back(new Entity("ZAZAZA"));
-    printf("ADD Entity %s \n", entitieList->at(2)->getID().c_str());
-
-    entitieList->push_back(new Entity(" GR GR GR "));
-    printf("ADD Entity %s \n", entitieList->at(3)->getID().c_str());
-
-
-    delete(entitieList);
-    printf("\n===================================================\n");
-
-    printf("\n===================Test Sequence and Work======================\n");
-    Entity* placeHoler = new Entity("PlaceHolder");
-    Entity* pm = new Entity("PM");
-    Work* startProcess = new Work("Start Work", 0);
-    Work* endProcess = new Work("End Work", 0);
-    Work* createDocument = new Work("CreateDocument", 10);
-    Sequence* startCreate = new Sequence("startWork", "1 Create Work", startProcess, createDocument);
-    Sequence* createEnd = new Sequence("endWork", "2 End Work", createDocument, endProcess);
-
-    startProcess->setEntity(placeHoler);
-    startProcess->setSequinceFrom(NULL);
-    startProcess->setSequinceTo(startCreate);
-
-    createDocument->setEntity(pm);
-    createDocument->setSequinceFrom(startCreate);
-    createDocument->setSequinceTo(createEnd);
-
-    endProcess->setEntity(placeHoler);
-    endProcess->setSequinceFrom(createEnd);
-    endProcess->setSequinceTo(NULL);
-
-    placeHoler->addWork(startProcess);
-    placeHoler->addWork(endProcess);
-
-    pm->addWork(createDocument);
-
-    printf("\n===================================================\n");
-    delete(pm);
-    delete(placeHoler);
-
-    //==================================Testing Core Save
+    //=================================Testing Core Save
     printf("\n=============================Core SAVE====================================\n");
     EntityFactory* ef = EntityFactory::entityFactory();
-    Entity* designer = ef->createEntity(TYPE_DESIGNER);
+
     Core* c = new Core();
-    c->addUserEntity(designer);
-    c->addWork("Make babushka",20.5,designer);
-    c->addUserEntity(new Entity("ZAZAZA2"));
-    c->addUserEntity(new Entity("ZAZAZA3"));
-    c->addUserEntity(new Entity("ZAZAZA4"));
-    c->addUserEntity(new Entity("ZAZAZA5"));
-    c->addUserEntity(new Entity("ZAZAZA6"));
-    c->addUserEntity(new Entity("ZAZAZA7"));
-    c->addUserEntity(new Entity("ZAZAZA8"));
-    c->addUserEntity(new Entity("ZAZAZA9"));
-    c->addUserEntity(new Entity("ZAZAZA0"));
+    Entity* pm = ef->createEntity(TYPE_PROJECT_MANAGER);
+    Entity* pr = ef->createEntity(TYPE_PROGRAMMER);
+    c->addUserEntity(pm);
+    c->addUserEntity(pr);
+    c->addWork("StartProject",0,pm);
+    c->addWork("Programming",40,pr);
+    c->addSequence("Create","Работать",
+                   c->getWorkByID("StartProject"),
+                   c->getWorkByID("Programming"));
+    c->addSequence("Ready","Отдать результат",
+                   c->getWorkByID("Programming"),
+                   c->getWorkByID("StartProject"));
     c->saveProject("MDP.SE-диаграмма");
     delete(c);
 
